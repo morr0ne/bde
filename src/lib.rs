221b6e9ad5660969b82b603af3f8b910f18fd026
Federico Maria Morrone <contact@morrone.dev>
2023-01-29 17:32:09 +0100

    Privated value modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod byte_string;
 mod de;
 mod error;
 mod ser;
-pub mod value;
+mod value;
 
 #[doc(inline)]
 pub use byte_string::ByteString;
@@ -13,4 +13,4 @@ pub use error::{Error, Result};
 #[doc(inline)]
 pub use ser::{to_bytes, to_writer, Serializer};
 #[doc(inline)]
-pub use value::{to_value, Dictionary, Integer, Value};
+pub use value::{to_value, Dictionary, Integer, Value, ValueSerializer};
